5096e73ab4de3bb159319777f08e4920cfb6f6ee
Owen Rummage <owen@rummage.cc>
2024-09-03 13:08:22 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,75 @@
+import datetime
+import json
+from flask import Flask, render_template
+from nut2 import PyNUTClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+app = Flask(__name__)
+
+json_data = """ 
+[ 
+    
+    {"name": "network_ups", "charge": 82, "load": 455, "state": "OL-CHG" , "runtime": 1200}, 
+    {"name": "prod_ups", "charge": 100, "load": 278, "state": "OL-CHD", "runtime": 3470 }
+
+]
+"""
+
+apc_units = json.loads(json_data)
+
+
+def getUnits():
+    client = PyNUTClient(host=os.getenv("HOST"))
+    return client.list_ups()
+
+
+def getUPSValues(name):
+    client = PyNUTClient(host=os.getenv("HOST"))
+
+    try:
+        if client.list_vars(name).get("output.voltage") is None:
+            print("Device {name} does not support OUTPUT values".format(name=name))
+            return {
+                "name": name, 
+                "runtime": int(client.list_vars(name).get("battery.runtime")),
+                "load": False,
+                "charge": int(client.list_vars(name).get("battery.charge")),
+                "status": client.list_vars(name).get("ups.status")
+            }
+        else:                 
+            return {
+                "name": name, 
+                "runtime": int(client.list_vars(name).get("battery.runtime")),
+                "load": float(client.list_vars(name).get("output.voltage"))*float(client.list_vars(name).get("output.current")),
+                "charge": int(client.list_vars(name).get("battery.charge")),
+                "status": client.list_vars(name).get("ups.status")
+            }
+    except Exception as error:
+        print(error)
+        print("Feces has impacted the wind circulation device for " + name)
+
+
+
+@app.route('/')
+def hello():
+    units = []
+    metrics = {
+        "totalUsage": 0,
+        "runtime": 200000000000000000000
+    }
+
+    for i in getUnits():
+        values = getUPSValues(i)
+
+        metrics["totalUsage"] = metrics["totalUsage"] + values.get("load")
+
+        if metrics["runtime"] > values.get("runtime"):
+            metrics["runtime"] = values.get("runtime")
+
+        units.append(values)
+
+    return render_template('index.html', units=units, metrics=metrics)
